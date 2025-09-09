59d96f7ed45a6903aba22f90c92754235acd2fc7
Coleson <coleson.smith@okstate.edu>
2025-09-09 13:33:48 -0500

    Add project files.

---
+++ CS_Basic_App_GitAzure/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace CS_Basic_App_GitAzure
+{
+    public class Class1
+    {
+        public string Name { get; }
+        public Class1()
+        {
+            Name = "[Coleson Smith] - The Code Master"; // Insert your name and moniker
+        }
+    }
+}
---
+++ CS_Basic_App_GitAzure/Pages/BasicPage.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace CS_Basic_App_GitAzure.Pages
+{
+    public class BasicPageModel : PageModel
+    {
+        public string Name { get; private set; }
+        public void OnGet()
+        {
+            Class1 tp = new Class1();
+            Name = tp.Name;
+        }
+    }
+}

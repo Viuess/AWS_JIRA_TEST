a3efe4f28a8d836a18c7efb2ef98eda5dfb90dbd
Coleson <viuess.business@gmail.com>
2025-11-11 10:35:15 -0600

    Added Courses

---
+++ CS_Basic_App_GitAzure/Pages/Courses.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CS_Basic_App_GitAzure.Pages
+{
+    public class CoursesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
